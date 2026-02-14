c3721c2a5fda08c665e05d8e3340740788cb39e8
xuenhan <xshe0033@student.monash.edu>
2026-02-13 22:34:30 -0500

    Implemented landing page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2045BA',
+        secondary: '#404040'
+      }
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
